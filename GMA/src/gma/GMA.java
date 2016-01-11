0b5ed3576817aad2b4b6cfd81820c8eaee5f41d1
dungnt <dungnt@192.168.37.86>
2016-01-11 11:20:22 +0700

    add new GMA

--- GMA/src/gma/GMA.java
+++ GMA/src/gma/GMA.java
@@ -5,6 +5,8 @@
  */
 package gma;
 
+import gma.network.GMAServer;
+
 /**
  *
  * @author dungnt
@@ -16,7 +18,7 @@ public class GMA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        GMAServer gmaServer = new GMAServer();
     }
     
 }
--- GMA/src/gma/network/GMAServer.java
+++ GMA/src/gma/network/GMAServer.java
@@ -5,10 +5,86 @@
  */
 package gma.network;
 
+import d88.core.network.D88ClientConnection;
+import d88.core.network.D88ServerNetwork;
+import d88.core.object.D88SObject;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.DataFormatException;
+
 /**
  *
  * @author dungnt
  */
-public class GMAServer {
+public class GMAServer implements D88ServerNetwork.D88NetworkDelegate{
+
+    public GMAServer() {
+        
+        D88ServerNetwork d88ServerNetwork = new D88ServerNetwork(1234, Executors.newCachedThreadPool(), this);
+        
+    }
+
+    @Override
+    public void onD88NetworkError(String message) {
+    }
+
+    @Override
+    public void ServerConnectionDidStart() {
+        System.out.println("SERVER DID START");
+    }
+
+    @Override
+    public void ServerConnectionStartFailure(String message) {
+    }
+
+    @Override
+    public void ServerConnectionDidEnd() {
+    }
+
+    @Override
+    public void ServerConnectionDidEndFailure(String message) {
+    }
+
+    @Override
+    public void ServerConnectionClientDidConnect(D88ClientConnection client) {
+    }
+
+    @Override
+    public void ServerConnectionClientConnectFailure(String message) {
+    }
+
+    @Override
+    public void clientdidReceiveMessage(byte[] message, D88ClientConnection formClient) {
+        try {
+            D88SObject d88SObject = new D88SObject(message);
+            System.out.println(d88SObject.getCmd());
+        } catch (DataFormatException ex) {
+            Logger.getLogger(GMAServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(GMAServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void clientConnectionLost(D88ClientConnection client) {
+    }
+
+    @Override
+    public void clientConnectionTimeout(D88ClientConnection client) {
+    }
+
+    @Override
+    public void clientdidDisconnect(D88ClientConnection client) {
+    }
+
+    @Override
+    public void clientDidSendMessage(byte[] message, D88ClientConnection client) {
+    }
+
+    @Override
+    public void clientdidSendMessageFailer(D88ClientConnection client, String message) {
+    }
+    
     
 }
